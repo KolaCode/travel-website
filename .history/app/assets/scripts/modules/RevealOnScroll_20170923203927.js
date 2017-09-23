ae41bbd5c294069d201414a5c2a1ce149f67e638
kola_just@yahoo.co.uk <kola_just@yahoo.co.uk>
2017-09-23 22:04:23 +0100

    Completed the reveal on scroll behaviour.

---
+++ .history/app/assets/scripts/modules/RevealOnScroll_20170923203927.js
@@ -0,0 +1,15 @@
+import $ from 'jquery';
+
+class RevealOnScroll {
+    constructor() {
+        this.itemsToReveal = $(".feature-item");
+        this.hideInitially();
+
+    }
+
+    hideInitially() {
+        this.itemsToReveal.addClass("reveal-items");
+    }
+}
+
+export default RevealOnScroll;
---
+++ .history/app/assets/scripts/modules/RevealOnScroll_20170923205251.js
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll {
+    constructor() {
+        this.itemsToReveal = $(".feature-item");
+        this.hideInitially();
+
+    }
+
+    hideInitially() {
+        this.itemsToReveal.addClass("reveal-item");
+    }
+
+    createWaypoints() {
+        this.itemsToReaveal.each(function() {
+            alert("testing");
+        });
+    }
+}
+
+export default RevealOnScroll;
